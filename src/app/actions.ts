72f409d536f1bc7cb3cc9d610fe34ab513af7eae
Tulio Gerbassi <tuliogerbassi@gmail.com>
2024-04-17 07:20:54 +0800

    condensed tag fetching and habit fetching to one func

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -2,30 +2,60 @@ import { Client } from "@notionhq/client";
 
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
 
-export async function fetchHabits(dbId) {
-  const allHabits = [];
-  let cursor = undefined;
+async function fetchItems(dbId, type) {
+  const allItems = [];
 
   try {
+    let cursor = undefined;
+
     while (true) {
-      const response = await notion.databases.query({
-        database_id: dbId,
-        start_cursor: cursor,
-        page_size: 100,
-      });
+      let response;
 
-      const { results, has_more, next_cursor } = response;
+      if (type === "tags") {
+        response = await notion.databases.retrieve({
+          database_id: dbId,
+        });
 
-      allHabits.push(...results);
+        response.properties.Tags.multi_select.options.forEach((item) =>
+          allItems.push(item.name)
+        );
 
-      if (!has_more) {
         break;
-      }
+      } else if (type === "habits") {
+        response = await notion.databases.query({
+          database_id: dbId,
+          start_cursor: cursor,
+          page_size: 100,
+        });
 
-      cursor = next_cursor;
+        const { results, has_more, next_cursor } = response;
+
+        allItems.push(...results);
+
+        if (!has_more) {
+          break;
+        }
+
+        cursor = next_cursor;
+      } else {
+        throw new Error("Invalid type specified");
+      }
     }
   } catch (error) {
-    console.error("Error fetching habits:", error);
+    console.error(`Error fetching ${type}:`, error);
+    return [];
   }
-  return allHabits;
+
+  return allItems;
+}
+
+export async function getHabits(dbId) {
+  try {
+    const tags = await fetchItems(dbId, "tags");
+    const habits = await fetchItems(dbId, "habits");
+    return { tags, habits };
+  } catch (error) {
+    console.error("Error getting habits:", error);
+    return { tags: [], habits: [] };
+  }
 }
